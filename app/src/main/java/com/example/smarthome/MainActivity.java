e4c0d59674e197215b47054a4cb9be215fdf03bd
zerodayy <38050593+zerodayy@users.noreply.github.com>
2022-03-05 16:06:05 +0200

    start queue

--- app/src/main/java/com/example/smarthome/MainActivity.java
+++ app/src/main/java/com/example/smarthome/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -53,7 +54,8 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(String response) {
                         // Display the first 500 characters of the response string.
-                        textView.setText("Response is: " + response.substring(0,500));
+                        Log.d("response", response);
+                        textView.setText("Response is: " + response);
                     }
                 }, new Response.ErrorListener() {
             @Override
